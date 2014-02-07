bef3467384bb2d0ec7d50bcdc75886b3f90ffbae
Kevin Peters <kwpeters@ra.rockwell.com>
2014-02-07 13:18:59 -0500

    Moved Git aliases to from Mac to common configuration.

--- bash_profile_common.bash
+++ bash_profile_common.bash
@@ -8,3 +8,24 @@
 ################################################################################
 
 echo 'Performing common initialization (bash_profile_common.bash)...'
+
+
+#
+# Git aliases
+#
+alias gitclean="git clean -d -f -x"
+alias gitbig="git config http.postBuffer 524288000"
+alias gitsmall="git config --unset http.postBuffer"
+
+
+#
+# copyFromGit
+#
+# A command that copies the source code out of a Git repository while
+# ignoring non-source code files.
+#
+copyFromGitFunc() {
+    kcopy.py --ignore "/\.git/" --ignore "/node_modules/" --ignore "\.DS_Store" $1 $2
+}
+
+alias copyFromGit=copyFromGitFunc
--- bash_profile_mac.bash
+++ bash_profile_mac.bash
@@ -91,11 +91,6 @@ alias grep='grep --color=auto'
 alias e="my_emacs"
 alias ec="my_emacsclient"
 
-# Git aliases
-alias gitclean="git clean -d -f -x"
-alias gitbig="git config http.postBuffer 524288000"
-alias gitsmall="git config --unset http.postBuffer"
-
 
 ################################################################################
 #
@@ -170,16 +165,3 @@ monitormdFunc() {
 alias monitormd=monitormdFunc
 
 
-################################################################################
-#
-# copyFromGit
-#
-# A command that copies the source code out of a Git repository while
-# ignoring non-source code files.
-#
-################################################################################
-copyFromGitFunc() {
-    kcopy.py --ignore "/\.git/" --ignore "/node_modules/" --ignore "\.DS_Store" $1 $2
-}
-
-alias copyFromGit=copyFromGitFunc
