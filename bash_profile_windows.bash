56591d54ae2f6c5d91b1f537d1616856fa590a1e
Kevin Peters <kwpeters@ra.rockwell.com>
2014-02-10 14:34:56 -0500

    Customized ls command on Windows.

--- bash_profile_windows.bash
+++ bash_profile_windows.bash
@@ -11,4 +11,7 @@ echo 'Performing Windows initialization (bash_profile_windows.bash)...'
 #
 # Use colors in the Terminal program.
 #
-alias ls='ls --color=auto'
+# --color=auto    Use color
+# -l              Long format
+# -A              Show all files (expect . and ..)
+alias ls='ls --color=auto -lA'
